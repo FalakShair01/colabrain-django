bd4eff874a72a9afd917765089e8b33df7defe04
FalakShair01 <falakshair.563@gmail.com>
2023-07-22 11:55:39 +0500

    initial commit

--- colabrain/settings.py
+++ colabrain/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-y^wfn9(psb7$*6&c8i^#*k-&j)%&3)xc1!)^q_ghe_tu510t4$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
